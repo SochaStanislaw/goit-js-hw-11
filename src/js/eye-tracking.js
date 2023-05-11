36c9bb00724fe1a8a0e0d0248c6f7c7b1dc3b19f
SochaStanislaw <socha.stanislaw95@gmail.com>
2023-05-11 21:54:30 +0200

    commit#6

--- src/js/eye-tracking.js
+++ src/js/eye-tracking.js
@@ -1,12 +1,12 @@
-// pupil trakcing pointer
+// pupil trakcs cursor
 const eyeTracking = document.querySelectorAll('.eye__white-area .eye__pupil');
 
 window.addEventListener('mousemove', e => {
   eyeTracking.forEach(pupil => {
     // get x and y postion of cursor
     var rect = pupil.getBoundingClientRect();
-    var x = (e.pageX - rect.left) / 75 + 'px';
-    var y = (e.pageY - rect.top) / 75 + 'px';
+    var x = (e.pageX - rect.left) / 65 + 'px';
+    var y = (e.pageY - rect.top) / 65 + 'px';
     pupil.style.transform = 'translate3d(' + x + ',' + y + ', 0px)';
   });
 });
